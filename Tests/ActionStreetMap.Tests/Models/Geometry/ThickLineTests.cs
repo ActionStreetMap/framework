e9e5d2968473a4829593d6908f383b9f5efc3f1d
Ilya Builuk <ilya.builuk@here.com>
2015-01-08 10:44:56 +0100

    ThickLine: improve unit test

--- Tests/ActionStreetMap.Tests/Models/Geometry/ThickLineTests.cs
+++ Tests/ActionStreetMap.Tests/Models/Geometry/ThickLineTests.cs
@@ -80,7 +80,9 @@ namespace ActionStreetMap.Tests.Models.Geometry
                 {
                     new MapPoint(5, 5), new MapPoint(10, 10),
                     new MapPoint(-10, 10), new MapPoint(-10, 20),
-                    new MapPoint(20, 20)
+                    new MapPoint(20, 20), new MapPoint(30, 30),
+                    new MapPoint(30, 130), new MapPoint(50, 130),
+                    new MapPoint(50, 50)
                 }, 5)
             };
 
@@ -88,15 +90,21 @@ namespace ActionStreetMap.Tests.Models.Geometry
             var result = ThickLineUtils.GetLineElementsInTile(leftBottomCorner, rightUpperCorner, lineElements);
 
             // ASSERT
-            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(3, result.Count);
             Assert.AreEqual(3, result[0].Points.Count);
             Assert.AreEqual(new MapPoint(5, 5), result[0].Points[0]);
             Assert.AreEqual(new MapPoint(10, 10), result[0].Points[1]);
             Assert.AreEqual(new MapPoint(0, 10), result[0].Points[2]);
 
-            Assert.AreEqual(2, result[1].Points.Count);
+            Assert.AreEqual(4, result[1].Points.Count);
             Assert.AreEqual(new MapPoint(0, 20), result[1].Points[0]);
             Assert.AreEqual(new MapPoint(20, 20), result[1].Points[1]);
+            Assert.AreEqual(new MapPoint(30, 30), result[1].Points[2]);
+            Assert.AreEqual(new MapPoint(30, 100), result[1].Points[3]);
+
+            Assert.AreEqual(2, result[2].Points.Count);
+            Assert.AreEqual(new MapPoint(50, 100), result[2].Points[0]);
+            Assert.AreEqual(new MapPoint(50, 50), result[2].Points[1]);
         }
     }
 }
