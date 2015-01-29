17a848669d22007ccab5e52d5f980789148c3149
Ilya Builuk <ilya.builuk@gmail.com>
2015-01-29 23:53:54 +0100

    Index: use IFileSystemService instead of System.IO.FileStream

--- ActionStreetMap.Maps/Index/Import/PersistentIndexBuilder.cs
+++ ActionStreetMap.Maps/Index/Import/PersistentIndexBuilder.cs
@@ -12,6 +12,7 @@ namespace ActionStreetMap.Maps.Index.Import
     {
         private readonly string _filePath;
         private readonly string _outputDirectory;
+        private readonly IFileSystemService _fileSystemService;
 
         public PersistentIndexBuilder(string filePath, string outputDirectory, IFileSystemService fileSystemService,
             ITrace trace)
@@ -19,21 +20,22 @@ namespace ActionStreetMap.Maps.Index.Import
         {
             _filePath = filePath;
             _outputDirectory = outputDirectory;
+            _fileSystemService = fileSystemService;
         }
 
         public override void Build()
         {
-            var sourceStream = new FileStream(_filePath, FileMode.Open);
+            var sourceStream = _fileSystemService.ReadStream(_filePath);
             var reader = GetReader(Path.GetExtension(_filePath), sourceStream);
 
             var kvUsageMemoryStream = new MemoryStream();
             var kvUsage = new KeyValueUsage(kvUsageMemoryStream);
 
-            var keyValueStoreFile = new FileStream(String.Format(Consts.KeyValueStorePathFormat, _outputDirectory), FileMode.Create);
+            var keyValueStoreFile = _fileSystemService.WriteStream(String.Format(Consts.KeyValueStorePathFormat, _outputDirectory));
             var index = new KeyValueIndex(Settings.Search.KvIndexCapacity, Settings.Search.PrefixLength);
             var keyValueStore = new KeyValueStore(index, kvUsage, keyValueStoreFile);
 
-            var storeFile = new FileStream(String.Format(Consts.ElementStorePathFormat, _outputDirectory), FileMode.Create);
+            var storeFile = _fileSystemService.WriteStream(String.Format(Consts.ElementStorePathFormat, _outputDirectory));
             Store = new ElementStore(keyValueStore, storeFile);
             Tree = new RTree<uint>(65);
 
@@ -41,20 +43,20 @@ namespace ActionStreetMap.Maps.Index.Import
             Clear();
             Complete();
 
-            using (var kvFileStream = new FileStream(String.Format(Consts.KeyValueUsagePathFormat, _outputDirectory), FileMode.Create))
+            using (var kvFileStream = _fileSystemService.WriteStream(String.Format(Consts.KeyValueUsagePathFormat, _outputDirectory)))
             {
                 var buffer = kvUsageMemoryStream.GetBuffer();
                 kvFileStream.Write(buffer, 0, (int) kvUsageMemoryStream.Length);
             }
 
-            KeyValueIndex.Save(index, new FileStream(String.Format(Consts.KeyValueIndexPathFormat, _outputDirectory), FileMode.Create));
-            SpatialIndex<uint>.Save(Tree, new FileStream(String.Format(Consts.SpatialIndexPathFormat, _outputDirectory), FileMode.Create));
+            KeyValueIndex.Save(index, _fileSystemService.WriteStream(String.Format(Consts.KeyValueIndexPathFormat, _outputDirectory)));
+            SpatialIndex<uint>.Save(Tree, _fileSystemService.WriteStream(String.Format(Consts.SpatialIndexPathFormat, _outputDirectory)));
             Store.Dispose();
         }
 
         public override void ProcessBoundingBox(BoundingBox bbox)
         {
-            using (var writer = new StreamWriter(new FileStream(String.Format(Consts.HeaderPathFormat, _outputDirectory), FileMode.Create)))
+            using (var writer = new StreamWriter(_fileSystemService.WriteStream(String.Format(Consts.HeaderPathFormat, _outputDirectory))))
             {
                 writer.Write("{0} {1}", bbox.MinPoint, bbox.MaxPoint);
             }
