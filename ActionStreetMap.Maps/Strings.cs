4d354f89dfb4b73b0bedb60be5a2de5c8ac1b384
Ilya Builuk <ilya.builuk@gmail.com>
2015-02-10 22:47:41 +0100

    Fixed broken unit test

--- ActionStreetMap.Maps/Strings.cs
+++ ActionStreetMap.Maps/Strings.cs
@@ -2,7 +2,7 @@
 {
     internal static class Strings
     {
-        public static string SearchNotSupported = "Search doesn't support given IElementSource implementation";
+        public static string SearchNotSupported = "Search doesn't support given IElementSource implementation (or it's null)";
         public static string NotSupportedMapFormat = "Unknown or not supported map format";
         public static string NoPresistentElementSourceFound = "No offline map data found for {0}, will query default server: {1}";
         public static string CannotFindSrtmData = "SRTM data cell not found: {0}";
--- Tests/ActionStreetMap.Tests/Maps/Index/SearchEngineTests.cs
+++ Tests/ActionStreetMap.Tests/Maps/Index/SearchEngineTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
+using ActionStreetMap.Core;
+using ActionStreetMap.Core.Tiling;
 using ActionStreetMap.Infrastructure.Dependencies;
+using ActionStreetMap.Infrastructure.Reactive;
 using ActionStreetMap.Maps.Data;
 using ActionStreetMap.Maps.Data.Search;
 using NUnit.Framework;
@@ -30,7 +33,12 @@ namespace ActionStreetMap.Tests.Maps.Index
         {
             // ARRANGE
             var componentRoot = TestHelper.GetGameRunner(_container);
+            var messageBus = _container.Resolve<IMessageBus>();
             componentRoot.RunGame(TestHelper.BerlinTestFilePoint);
+
+            // NOTE wait for tile loading ends before ask search engine
+            messageBus.AsObservable<TileLoadFinishMessage>().Take(1).Wait();
+
             var searchEngine = _container.Resolve<ISearchEngine>();
 
             // ACT
