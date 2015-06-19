71c84ef43d627f86cbee6670e534ed7cd8cc8d58
Ilya Builuk <ilya.builuk@gmail.com>
2015-06-19 16:06:27 +0200

    Editor: add tile model editor class

---
+++ ActionStreetMap.Explorer/Tiling/TileModelEditor.cs
@@ -0,0 +1,35 @@
+﻿using ActionStreetMap.Core.Scene;
+using ActionStreetMap.Core.Tiling;
+using ActionStreetMap.Maps.Data;
+
+namespace ActionStreetMap.Explorer.Tiling
+{
+    /// <summary> Provides the way to edit tile models. </summary>
+    public interface ITileModelEditor
+    {
+        /// <summary> Adds building to current scene. </summary>
+        void AddBuilding(Building building);
+    }
+
+    /// <summary> Default implementation of <see cref="ITileModelEditor"/>. </summary>
+    internal sealed class TileModelEditor : ITileModelEditor
+    {
+        private readonly ITileController _tileController;
+        private readonly IElementSourceEditor _elementSourceEditor;
+
+        /// <summary> Creates instance of <see cref="TileModelEditor"/>. </summary>
+        /// <param name="tileController">Tile controller. </param>
+        /// <param name="elementSourceEditor">Element source editor.</param>
+        public TileModelEditor(ITileController tileController, IElementSourceEditor elementSourceEditor)
+        {
+            _tileController = tileController;
+            _elementSourceEditor = elementSourceEditor;
+        }
+
+        /// <inheritdoc />
+        public void AddBuilding(Building building)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
