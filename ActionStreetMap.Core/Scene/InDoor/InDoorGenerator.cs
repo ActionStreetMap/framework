1c74e40cb4cfe038231d7563cb0096f01e17f4bd
Ilya Builuk <ilya.builuk@gmail.com>
2015-07-17 21:50:48 +0200

    Refactoring: extract indoor generator settings to separate class

--- ActionStreetMap.Core/Scene/InDoor/InDoorGenerator.cs
+++ ActionStreetMap.Core/Scene/InDoor/InDoorGenerator.cs
@@ -5,37 +5,10 @@ using ActionStreetMap.Core.Geometry;
 using ActionStreetMap.Core.Geometry.Clipping;
 using ActionStreetMap.Core.Geometry.StraightSkeleton;
 using ActionStreetMap.Core.Geometry.Utils;
-using ActionStreetMap.Infrastructure.Utilities;
 
 namespace ActionStreetMap.Core.Scene.InDoor
 {
-    internal class InDoorGeneratorSettings
-    {
-        public double MinimalWidthStep = 10;
-        public double PreferedWidthStep = 100;
-        public double VaaSizeHeight = 20;
-        public double VaaSizeWidth = 40; // along skeleton edge
-
-        public readonly double HalfTransitAreaWidth;
-        public readonly double TransitAreaWidth;
-        public readonly double MinimalArea;
-
-        public readonly IObjectPool ObjectPool;
-        public readonly Clipper Clipper;
-
-        public InDoorGeneratorSettings(IObjectPool objectPool, Clipper clipper,
-            double transitAreaWidth)
-        {
-            ObjectPool = objectPool;
-            Clipper = clipper;
-
-            TransitAreaWidth = transitAreaWidth;
-            HalfTransitAreaWidth = transitAreaWidth/2;
-            MinimalArea = TransitAreaWidth*TransitAreaWidth;
-        }
-    }
-
-    internal class InDoorGenerator
+    internal sealed class InDoorGenerator
     {
         private const int InvalidIndex = -1;
         private const int Scale = 1000;
---
+++ ActionStreetMap.Core/Scene/InDoor/InDoorGeneratorSettings.cs
@@ -0,0 +1,31 @@
+﻿using ActionStreetMap.Core.Geometry.Clipping;
+using ActionStreetMap.Infrastructure.Utilities;
+
+namespace ActionStreetMap.Core.Scene.InDoor
+{
+    internal sealed class InDoorGeneratorSettings
+    {
+        public double MinimalWidthStep = 10;
+        public double PreferedWidthStep = 100;
+        public double VaaSizeHeight = 20;
+        public double VaaSizeWidth = 40; // along skeleton edge
+
+        public readonly double HalfTransitAreaWidth;
+        public readonly double TransitAreaWidth;
+        public readonly double MinimalArea;
+
+        public readonly IObjectPool ObjectPool;
+        public readonly Clipper Clipper;
+
+        public InDoorGeneratorSettings(IObjectPool objectPool, Clipper clipper,
+            double transitAreaWidth)
+        {
+            ObjectPool = objectPool;
+            Clipper = clipper;
+
+            TransitAreaWidth = transitAreaWidth;
+            HalfTransitAreaWidth = transitAreaWidth/2;
+            MinimalArea = TransitAreaWidth*TransitAreaWidth;
+        }
+    }
+}
