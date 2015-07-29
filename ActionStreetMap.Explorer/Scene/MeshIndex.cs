85ea12b1a21e631251d000cd674e60723050a182
Ilya Builuk <ilya.builuk@gmail.com>
2015-07-29 14:42:40 +0200

    Add plane mesh index

---
+++ ActionStreetMap.Explorer/Scene/MeshIndex.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using UnityEngine;
+
+namespace ActionStreetMap.Explorer.Scene
+{
+    /// <summary> Represents index of mesh vertices for quick search. </summary>
+    public interface IMeshIndex
+    {
+        /// <summary> Builds index. </summary>
+        void Build();
+
+        /// <summary> Modifies mesh using query provided. </summary>
+        bool Modify(MeshQuery query);
+    }
+
+    /// <summary> Mesh index for plane. </summary>
+    internal class PlaneMeshIndex : IMeshIndex
+    {
+        /// <summary> Normal vector to plane. </summary>
+        protected readonly Vector3 N;
+
+        /// <summary> Magnitude of normal vector. </summary>
+        protected readonly float NormalMagnitude;
+
+        /// <summary> Coefficient from plane equation. </summary>
+        protected readonly float D;
+
+        /// <summary> Initializes index using three points on the plane. </summary>
+        public PlaneMeshIndex(Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            N = Vector3.Cross(p2 - p1, p3 - p1);
+            NormalMagnitude = N.magnitude;
+            D = p1.x*N.x + p1.y*N.y + p1.z*N.z;
+        }
+
+        /// <inheritdoc />
+        public void Build()
+        {
+            // TODO try to create some index internally to avoid iteration
+            // over whole collection of vertices
+        }
+
+        /// <inheritdoc />
+        public bool Modify(MeshQuery query)
+        {
+            var vertices = query.Vertices;
+            bool isModified = false;
+            for (int j = 0; j < vertices.Length; j += 3)
+            {
+                // triangle is already collapsed
+                if (vertices[j] == vertices[j + 1])
+                    continue;
+
+                for (int i = j; i < j + 3; i++)
+                {
+                    var v = vertices[i];
+                    var distanceToCollidePoint = Vector3.Distance(v, query.CollidePoint);
+                    if (distanceToCollidePoint < query.Radius)
+                    {
+                        isModified = true;
+                        var distanceToWall = (v.x * N.x + v.y * N.y + v.z * N.z - D) / NormalMagnitude;
+                        var forceChange = query.ForcePower * (query.Radius - distanceToCollidePoint) / 2;
+
+                        // NOTE whole traingle should be removed as one of the vertices is 
+                        // moved more than threshold allows
+                        if (Math.Abs(distanceToWall + forceChange) > query.OffsetThreshold)
+                        {
+                            // collapse triangle into point
+                            var firstVertIndex = i - i % 3;
+                            vertices[firstVertIndex + 1] = vertices[firstVertIndex];
+                            vertices[firstVertIndex + 2] = vertices[firstVertIndex];
+                            break;
+                        }
+                        vertices[i] += forceChange * query.ForceDirection;
+                    }
+                }
+            }
+            return isModified;
+        }
+    }
+
+    /// <summary> Represents mesh index which does nothing. </summary>
+    internal sealed class DummyMeshIndex : IMeshIndex
+    {
+        public static readonly  DummyMeshIndex Default = new DummyMeshIndex();
+        
+        private DummyMeshIndex() { }
+
+        /// <inheritdoc />
+        public void Build()
+        {
+        }
+
+        /// <inheritdoc />
+        public bool Modify(MeshQuery query)
+        {
+            return false;
+        }
+    }
+}
---
+++ ActionStreetMap.Explorer/Scene/MeshQuery.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace ActionStreetMap.Explorer.Scene
+{
+    /// <summary> Represents DTO which is used to modify mesh. </summary>
+    public class MeshQuery
+    {
+        /// <summary> Epicenter of modification. </summary>
+        public Vector3 Epicenter;
+
+        /// <summary> Radious of modification. </summary>
+        public float Radius;
+
+        /// <summary> Offset threshold used to destroy triangles. </summary>
+        public float OffsetThreshold;
+
+        /// <summary> Force power. </summary>
+        public float ForcePower;
+
+        /// <summary> Force direction. </summary>
+        public Vector3 ForceDirection;
+
+        /// <summary> Collide point on mesh </summary>
+        public Vector3 CollidePoint;
+
+        /// <summary> Mesh vertices. </summary>
+        public Vector3[] Vertices;
+    }
+}
