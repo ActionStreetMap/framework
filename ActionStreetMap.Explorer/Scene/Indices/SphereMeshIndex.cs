8292fe387b2f1ab01a81e6d623c7a76a6a663fca
Ilya Builuk <ilya.builuk@gmail.com>
2015-08-02 23:19:13 +0200

    Mesh index: add initial state of sphere mesh index and unit test for it

---
+++ ActionStreetMap.Explorer/Scene/Indices/SphereMeshIndex.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace ActionStreetMap.Explorer.Scene.Indices
+{
+    /// <summary> Mesh index for sphere. </summary>
+    internal class SphereMeshIndex : IMeshIndex
+    {
+        private readonly float _radius;
+        private readonly Vector3 _center;
+
+        /// <summary> Creates instance of <see cref="SphereMeshIndex"/>. </summary>
+        public SphereMeshIndex(float radius, Vector3 center)
+        {
+            _radius = radius;
+            _center = center;
+        }
+
+        /// <inheritdoc />
+        public void Build()
+        {
+        }
+
+        /// <inheritdoc />
+        public MeshQuery.Result Modify(MeshQuery query)
+        {
+            return null;
+        }
+    }
+}
---
+++ Tests/ActionStreetMap.Tests/Explorer/Scene/Indices/SphereMeshIndexTests.cs
@@ -0,0 +1,55 @@
+﻿using ActionStreetMap.Explorer.Scene;
+using ActionStreetMap.Explorer.Scene.Generators;
+using ActionStreetMap.Explorer.Scene.Indices;
+using ActionStreetMap.Explorer.Utils;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace ActionStreetMap.Tests.Explorer.Scene.Indices
+{
+    [TestFixture]
+    class SphereMeshIndexTests
+    {
+        private const float Radius = 5;
+        private readonly Vector3 Center = new Vector3(0, 0, 0);
+
+        [Test]
+        public void CanMakeQuery()
+        {
+            // ARRANGE
+            var vertices = GetVertices();
+            var query = new MeshQuery()
+            {
+                Epicenter = new Vector3(0, Radius, 0),
+                ForceDirection = new Vector3(0, -1, 0),
+                GetForceChange = f => 1,
+                Vertices = vertices,
+                Radius = Radius,
+                OffsetThreshold = 1
+            };
+
+            // ACT
+            var meshIndex = new SphereMeshIndex(Radius, Center);
+            meshIndex.Build();
+            var result = meshIndex.Modify(query);
+
+            // ASSERT
+            Assert.IsNotNull(result);
+        }
+
+        private Vector3[] GetVertices()
+        {
+            var meshData = new MeshData();
+            var sphereGen = new IcoSphereGenerator(meshData)
+                .SetCenter(Center)
+                .SetRadius(Radius)
+                .SetRecursionLevel(2)
+                .SetGradient(GradientUtils.ParseGradient("gradient(#808080, #606060 50%, #505050)"));
+
+            meshData.Initialize(sphereGen.CalculateVertexCount());
+            sphereGen.Build();
+
+            return meshData.Vertices;
+        }
+    }
+}
